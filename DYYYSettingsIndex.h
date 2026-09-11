8a128bcd9fe7ad10943c703a4d8d5c2e070fde6b
xingyou9 <165631185+xingyou9@users.noreply.github.com>
2026-09-11 13:44:06 +0800

    fix: DYYYSettingsSearchIndex 加 extern C（.xm C++ 链接名不一致）

--- DYYYSettingsIndex.h
+++ DYYYSettingsIndex.h
@@ -2,6 +2,10 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * 全局设置搜索索引（自动生成，勿手改）
  *
@@ -9,5 +13,8 @@ NS_ASSUME_NONNULL_BEGIN
  * 重新生成：修改设置页后运行提取脚本即可。
  */
 NSArray<NSDictionary<NSString *, id> *> *DYYYSettingsSearchIndex(void);
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 NS_ASSUME_NONNULL_END
